7aa4e2cd08e6324db81a4645974813a8c87b9731
hhkim331 <97862258+hhkim331@users.noreply.github.com>
2023-11-08 20:26:41 +0900

    color picker

---
+++ Assets/KHH/1.Scripts/Custom/KHHUICategoryItem.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KHHUICategoryItem : MonoBehaviour
+{
+    [SerializeField] private Text m_CategoryTitle;
+    [SerializeField] private Text m_EquipedItemTitle;
+    [SerializeField] private Button m_Button;
+
+    public System.Action OnClick;
+
+    public string Title
+    {
+        get => m_CategoryTitle.text;
+        set => m_CategoryTitle.text = value;
+    }
+
+    public string Value
+    {
+        get => m_EquipedItemTitle.text;
+        set => m_EquipedItemTitle.text = value;
+    }
+
+    private void Awake()
+    {
+        m_Button.onClick.AddListener(Callback_OnClick);
+    }
+
+    private void Callback_OnClick()
+    {
+        OnClick?.Invoke();
+    }
+}
---
+++ Assets/KHH/1.Scripts/Custom/KHHUIColorPicker.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UI.Extensions.ColorPicker;
+
+public class KHHUIColorPicker : MonoBehaviour
+{
+    //[SerializeField] Button[] colorButtons;
+    [SerializeField] ColorPickerControl colorPicker;
+    [Space]
+    [SerializeField] private Button m_CloseButton;
+
+    public System.Action<Color> OnChangeColor;
+
+    private Color m_Color;
+
+    private void Awake()
+    {
+        m_CloseButton.onClick.AddListener(OnClickClose);
+    }
+
+    public void Show(Color color, System.Action<Color> onChangeColor)
+    {
+        if (gameObject.activeSelf)
+            Close();
+
+        colorPicker.CurrentColor = color;
+        colorPicker.onValueChanged.AddListener((c) => OnChangeColor?.Invoke(c));
+        //foreach (var b in colorButtons)
+        //    b.onClick.RemoveAllListeners();
+
+        m_Color = color;
+        OnChangeColor = onChangeColor;
+
+        gameObject.SetActive(true);
+    }
+
+    public void Close()
+    {
+        OnChangeColor = null;
+        gameObject.SetActive(false);
+    }
+
+    //private void OnColorButtonClick(int idx)
+    //{
+    //    m_Color = colorButtons[idx].image.color;
+    //    OnChangeColor?.Invoke(m_Color);
+    //}
+
+    private void OnClickClose()
+    {
+        this.Close();
+    }
+
+    //private void OnValidate()
+    //{
+    //    for (int i = 0; i < colorButtons.Length; i++)
+    //        if (colorButtons[i].onClick.GetPersistentEventCount() == 0)
+    //            UnityEventTools.AddIntPersistentListener(colorButtons[i].onClick, OnColorButtonClick, i);
+    //}
+}
