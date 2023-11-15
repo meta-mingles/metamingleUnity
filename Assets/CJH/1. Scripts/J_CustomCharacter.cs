04893dc31f5517606d6380228f61a8b855f11e79
JCURVEs <jaehee2173@gmail.com>
2023-11-15 19:29:00 +0900

    push

---
+++ Assets/CJH/1. Scripts/J_CustomCharacter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class J_CustomCharacter : MonoBehaviour
+{
+    void Start()
+    {
+        //load data
+        KHHUserCustom.Init();
+        KHHUserCustomData data = KHHUserCustom.LoadData();
+    }
+
+    void Update()
+    {
+        
+    }
+}
