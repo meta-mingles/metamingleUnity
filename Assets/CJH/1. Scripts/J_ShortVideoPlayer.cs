2e64bb36387f52df09258ebbe4ebb8a99a9d0132
JCURVEs <jaehee2173@gmail.com>
2023-11-27 08:51:48 +0900

    push

--- Assets/CJH/1. Scripts/J_ShortVideoPlayer.cs
+++ Assets/CJH/1. Scripts/J_ShortVideoPlayer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections;
 using TMPro;
 using UnityEngine;
@@ -16,38 +17,31 @@ public class J_ShortVideoPlayer : J_VideoPlayerBase
     public TMP_Text like_Count;
     public RawImage profilecolor;
     public Button like_Button; // 좋아요 버튼
-    public Button Sound_Button; //사운드 버튼
     public float colorChangeDuration = 0.5f;//색상변경시간
-    int likeCnt;
     private Color[] rainbowColor = new Color[] { Color.green, Color.magenta, Color.yellow, Color.blue, Color.cyan, Color.red }; //무지개 색상 변화
     public J_InteractiveMovieItem interactiveMovieList;
-    private void Start()
+    public Action onColorChangeComplete;
+    private void Awake()
     {
-        like_Button.onClick.AddListener(() => StartCoroutine(ChangeToRainbowAndThenBlack()));
-        //처음엔 0 
-        like_Count.text = "0";
-        //but 누르고나서 서버에서 받는 좋아요수로 업데이트
         if (like_Button != null)
         {
             like_Button.onClick.AddListener(LikeButton);
         }
+        like_Button.onClick.AddListener(() => StartCoroutine(ChangeToRainbowAndThenBlack()));
     }
     //숏폼 비디오 서버 
     public override void SetItem(ShortVideoInfo info)
     {
         base.SetItem(info);
-
         //제목
         title.text = videoInfo.title;
         //날짜 
         string[] temp = videoInfo.date.Split('T');
         date.text  = "DATE : " + temp[0];
-
         //영상 설명
         description.text = "DESCRIPTION : " + videoInfo.description;
         //크리에이터
         membername.text = videoInfo.memberName;
-
         //좋아요 수
         like_Count.text = videoInfo.shortFormLikeCnt.ToString();
     }
@@ -75,20 +69,29 @@ public class J_ShortVideoPlayer : J_VideoPlayerBase
     }
     void LikeButton()
     {
-        likeCnt = videoInfo.shortFormLikeCnt;
-        likeCnt += 1;
-        like_Count.text = likeCnt.ToString();
-
-        //계정당 한번
-        if (videoInfo.isLike == true) //숏폼을 좋아요 했다면
+        int likeCnt = videoInfo.shortFormLikeCnt;
+        //좋아요 안 한 영상이라면
+        if (!videoInfo.isLike)
         {
-            return;
+            // HttpInfo 인스턴스 생성 및 설정
+            HttpInfo httpInfo = new HttpInfo();
+            string url = "/short-form/" + videoInfo.shortFormNo + "/like"; // 숏폼 번호를 URL에 포함
+            httpInfo.Set(RequestType.POST, url, (DownloadHandler downloadHandler) =>
+            {
+                JObject jObject = JObject.Parse(downloadHandler.text);
+                ShortVideoInfo data = jObject["data"].ToObject<ShortVideoInfo>();
+                bool isLike = jObject["data"].ToObject<bool>();
+                int like_Count = jObject["data"].ToObject<int>();
+
+            });
+            // HttpManager를 통해 요청 보내기
+            HttpManager.instance.SendRequest(httpInfo);
         }
-        else
+        else //좋아요 한 영상이라면
         {
 
         }
-        //서버랑 연동
+
     }
 
     //무지개색상변경
@@ -115,5 +118,9 @@ public class J_ShortVideoPlayer : J_VideoPlayerBase
             yield return null;
         }
         image.color = newColor;
+
+        // 색상 변경이 완료되면 콜백 함수 호출
+        onColorChangeComplete?.Invoke();
+
     }
 }
