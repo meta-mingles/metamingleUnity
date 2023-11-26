2e91a13b1699cc39b531e6249c0ebe3b653e194a
JCURVEs <jaehee2173@gmail.com>
2023-11-26 21:55:43 +0900

    번역 기능 완료

--- Assets/CJH/1. Scripts/PhotonChatItem.cs
+++ Assets/CJH/1. Scripts/PhotonChatItem.cs
@@ -1,18 +1,32 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Globalization;
+using WebSocketSharp;
 
 public class PhotonChatItem : MonoBehaviour
 {
     public TextMeshProUGUI nicknameText;
     public TextMeshProUGUI myText;
+    public TextMeshProUGUI outputText;
     public Button translationBt;
+
+    private bool isTraslate = false;
     RectTransform rt;
+
+    public Test_deepl translator;
+
     // Start is called before the first frame update
     void Awake()
     {
+        translator = FindObjectOfType<Test_deepl>();
+        if (translator == null)
+        {
+            Debug.LogError("Test_deepl instance not found in the scene.");
+        }
         translationBt.onClick.AddListener(Translate);
         rt = GetComponent<RectTransform>();
     }
@@ -21,16 +35,78 @@ public class PhotonChatItem : MonoBehaviour
         nicknameText.text = nickname;
         myText.text = chat;
         myText.color = color;
-
+       
+        
         //text의 height를 적정크기로 설정
         rt.sizeDelta = new Vector2(rt.sizeDelta.x, myText.preferredHeight);
     }
-    public async void Translate()
+    async void Translate()
     {
-        string currentLanguage = await TranslationManager.instance.DetectLanguage(myText.text);
-        string targetLanguage = currentLanguage == "EN" ? "KO" : "EN";
-        string translatedText = await TranslationManager.instance.TranslateText(myText.text, targetLanguage);
-        myText.text = translatedText;
+        //원본언어상태
+        if (!isTraslate)
+        {
+            //번역을 한적이 없다
+            if (string.IsNullOrEmpty(outputText.text))
+            {
+                if (Test_deepl.Instance != null)
+                {
+                    isTraslate = true;
+                    string currentLanguage = DetermineLanguage(myText.text);
+
+                    if (currentLanguage == "ko")
+                    {
+                        string translatedText = await translator.StartTranslation(myText.text, "EN-GB");
+                        outputText.text = translatedText;
+                        myText.gameObject.SetActive(false);
+                        outputText.gameObject.SetActive(true);
+
+                    }
+                    else if (currentLanguage == "en")
+                    {
+                        string translatedText = await translator.StartTranslation(myText.text, "ko");
+                        outputText.text = translatedText;
+                        myText.gameObject.SetActive(false);
+                        outputText.gameObject.SetActive(true);
+                    }
+                }
+                else
+                {
+                    Debug.Log("번역 실행안됌");
+                }
+            }
+            //번역을 한적이 있다.
+            else
+            {
+                myText.gameObject.SetActive(false);
+                outputText.gameObject.SetActive(true);
+                isTraslate = true;
+            }
+        }
+        //번역 언어 상태
+        else
+        {
+            //번역 이전의 값으로 돌아간다
+            myText.gameObject.SetActive(true);
+            outputText.gameObject.SetActive(false);
+            isTraslate = false;
+        }
+    }
+    private void ReturnTranslate()
+    {
+
     }
 
+
+    private string DetermineLanguage(string text)
+    {
+        foreach(char c in text)
+        {
+            if(char.GetUnicodeCategory(c) == UnicodeCategory.OtherLetter)
+            {
+                return "ko";
+            }
+
+        }
+        return "en";
+    }
 }
--- Assets/Yechan/Script/Test_deepl.cs
+++ Assets/Yechan/Script/Test_deepl.cs
@@ -4,9 +4,11 @@ using TMPro;
 using DeepL;
 using System.Threading.Tasks;
 using Unity.VisualScripting;
+using System;
 
 public class Test_deepl : MonoBehaviour
 {
+    public static Test_deepl Instance { get; private set; }
     public Change_language get_language;
     private Translator translator;
     public Button KEButton;
@@ -15,22 +17,48 @@ public class Test_deepl : MonoBehaviour
     public TMP_InputField outputText;
     string input_language;
     string output_language;
-
-    async void StartTranslation(string text, string selected_language)
+    void Awake()
     {
-        string translatedText = await Translate(text);
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public async Task<string> StartTranslation(string text,string selected_language)
+    {
+        string translatedText = await Translate(text,selected_language);
         outputText.text = translatedText;
         Debug.Log("번역된 텍스트: " + translatedText);
+        return translatedText;
     }
-    async Task<string> Translate(string text)
+    async Task<string> Translate(string text,string selected_language)
     {
-        var translations = await translator.TranslateTextAsync(
-        new[] { text }, null, selected_language);
-
-        Debug.Log("식별된 언어");
-        Debug.Log(translations[0].DetectedSourceLanguageCode); // "JA"
-
-        return translations[0].Text;
+        Debug.Log("Selected language before assignment: " + selected_language);
+        try
+        {
+            var translations = await translator.TranslateTextAsync(new[] { text }, null, selected_language);
+            Debug.Log("Selected language after assignment: " + selected_language);
+            if (translations != null && translations.Length > 0)
+            {
+                return translations[0].Text;
+            }
+            else
+            {
+                Debug.LogError("No translation received.");
+                return null;
+            }
+        }
+        catch (Exception ex)
+        {
+            Debug.LogError("Translation error: " + ex.Message);
+            return null;
+        }
     }
     string selected_language;
     void KEOnButtonClicked()
@@ -49,7 +77,7 @@ public class Test_deepl : MonoBehaviour
         string textValue = inputText.text;
 
         Debug.Log("Button Clicked! Text is: " + textValue);
-        StartTranslation(textValue, selected_language);
+        StartTranslation(textValue,selected_language);
     }
     void Start()
     {
